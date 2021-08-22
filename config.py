e3535202aee4277cb0691cae568c541cfa5566a6
Adrian Krol <adriankrol11@gmail.com>
2021-08-22 14:26:17 +0200

    add database

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
-class Config:
-   SECRET_KEY = os.environ.get("SECRET_KEY") or "asjdbhGHBFASn"
-   SQLALCHEMY_DATABASE_URI = (                           
-           os.environ.get('DATABASE_URL') or
-           'sqlite:///' + os.path.join(BASE_DIR, 'book_catalogue.db')
-   )
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-   template_dir = os.path.join(BASE_DIR, "templates")
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "asjdbhGHBFASn"
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') or
+        'sqlite:///' + os.path.join(BASE_DIR, 'post_database.db')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    template_dir = os.path.join(BASE_DIR, "templates")
