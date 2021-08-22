0a4ec402b254e4401d1216cab69ec0c184a502f0
Adrian Krol <adriankrol11@gmail.com>
2021-08-22 14:39:50 +0200

    add read posts page

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from blog import app
+from blog.models import Entry
 
-@app.route('/')
-def hello():
-    return render_template("base.html")
+
+@app.route("/")
+def index():
+    all_posts = Entry.query.filter_by(
+        is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
