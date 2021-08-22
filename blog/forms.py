36812b66272eb8eeab01fcb13c55e09bd758fadb
Adrian Krol <adriankrol11@gmail.com>
2021-08-22 18:42:56 +0200

    add editing posts

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class EntryForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": "Title"})
-    content = TextAreaField('Post content', validators=[DataRequired()], render_kw={"placeholder": "Post content"})
-    published = BooleanField('Published')
-    
+    title = StringField('Title', validators=[DataRequired()], render_kw={
+                        "placeholder": "Title"})
+    body = TextAreaField('Post content', validators=[DataRequired()], render_kw={
+                         "placeholder": "Post content"})
+    is_published = BooleanField('Published')
--- blog/routes.py
+++ blog/routes.py
@@ -10,21 +10,38 @@ def homepage():
         is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts=all_posts)
 
+
 @app.route("/new-post/", methods=["GET", "POST"])
 def create_entry():
-   form = EntryForm()
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           entry = Entry(
-               title=form.title.data,
-               body=form.content.data,
-               is_published=form.published.data
-           )
-           db.session.add(entry)
-           db.session.commit()
-           flash('You were successfully logged in')
-           return redirect(url_for('create_entry'))
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+            flash('Pomyślnie dodano post')
+            return redirect(url_for('create_entry'))
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(entry)
+            db.session.commit()
+            flash('Pomyślnie edytowano post')
+            return redirect(url_for('homepage'))
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
