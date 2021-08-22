6b1cd7fdf1a602a370af72af9140192191e59156
Adrian Krol <adriankrol11@gmail.com>
2021-08-22 18:57:13 +0200

    refactor of edit/create functions

--- blog/routes.py
+++ blog/routes.py
@@ -10,38 +10,43 @@ def homepage():
         is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts=all_posts)
 
-
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
+def edit_post(method, entry_id = None):
+    entry = []
+    form = []
+    if entry_id:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+    else:
+        form = EntryForm()
     errors = None
-    if request.method == 'POST':
+    if method == 'POST':
         if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
+            if entry_id:
+                form.populate_obj(entry)
+                flash('Pomyślnie edytowano post')
+            else:
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
+                flash('Pomyślnie dodano post')
+                
             db.session.commit()
-            flash('Pomyślnie dodano post')
-            return redirect(url_for('create_entry'))
-        else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
-
-
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            flash('Pomyślnie edytowano post')
+            
             return redirect(url_for('homepage'))
         else:
             errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return edit_post(method = request.method)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    return edit_post(entry_id=entry_id,method = request.method)
